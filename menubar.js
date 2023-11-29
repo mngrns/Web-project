c36cb50168dd767c5f50ff4d21031dc08c9804d8
hism23155 <24286479-hism23155@users.noreply.replit.com>
2023-11-29 22:41:31 +0000

    Initial commit

---
+++ menubar.js
@@ -0,0 +1,8 @@
+const open = document.getElementById('open');
+
+const overlay = document.querySelector('.overlay');
+
+open.addEventListener('click', () => {
+  overlay.classList.add('show');
+  open.classList.add('hide');
+});
